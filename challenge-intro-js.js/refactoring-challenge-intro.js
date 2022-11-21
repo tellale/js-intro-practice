6870922accd3fd83c6de62cd30ba50551b5000d0
tellale <alejandraterego@gmail.com>
2022-11-21 15:51:36 +0100

    done all must-have requirements and finish script workflow

--- challenge-intro-js.js/refactoring-challenge-intro.js
+++ challenge-intro-js.js/refactoring-challenge-intro.js
@@ -1,12 +1,12 @@
 import readline from 'readline';
-import { 
-  printTable, 
-  fullStudentsLength, 
-  allStudentsName, 
-  deleteLastStudent, 
-  deleteRandomStudent, 
-  eachGirlData, 
-  totalPerGender, 
+import {
+  printTable,
+  fullStudentsLength,
+  allStudentsName,
+  deleteLastStudent,
+  deleteRandomStudent,
+  eachGirlData,
+  totalPerGender,
   allGirlsBool,
   allYoungAdults,
   addRandomStudent,
@@ -15,36 +15,25 @@ import {
   averageGirlsAge,
   addRandomScore,
   sortAlphabetically
- } from './functions.js'
-
-
-  // Ejercicio:
-
-// Una vez la ejecución termine, volveremos a mostrar el listado de requisitos para que el usuario pueda seleccionar otro. 
-// pulse el 0 o un número no contemplado, la aplicación terminará.
+} from './functions.js'
 
 // Requisitos para imprimir
 const Requisitos = {
-1: 'Mostrar en formato de tabla todos los alumnos.',
-2: 'Mostrar por consola la cantidad de alumnos que hay en clase.',
-3: 'Mostrar por consola todos los nombres de los alumnos.',
-4: 'Eliminar el último alumno de la clase.',
-5: 'Eliminar un alumno aleatoriamente de la clase.',
-6: 'Mostrar por consola todos los datos de los alumnos que son chicas.',
-7: 'Mostrar por consola el número de chicos y chicas que hay en la clase.',
-8: 'Mostrar true o false por consola si todos los alumnos de la clase son chicas.',
-9: 'Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.',
-10: 'Añadir un alumno nuevo',
-11: 'Mostrar por consola el nombre de la persona más joven de la clase.',
-12: 'Mostrar por consola la edad media de todos los alumnos de la clase.',
-13: 'Mostrar por consola la edad media de las chicas de la clase.',
-14: 'Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10)',
-15: 'Ordenar el array de alumnos alfabéticamente según su nombre.'
-}
-
-// aparecerá el listado completo de requisitos.
-for (const key of Object.keys(Requisitos)) {
-    console.log(key + " : " + Requisitos[key])
+  1: 'Mostrar en formato de tabla todos los alumnos.',
+  2: 'Mostrar por consola la cantidad de alumnos que hay en clase.',
+  3: 'Mostrar por consola todos los nombres de los alumnos.',
+  4: 'Eliminar el último alumno de la clase.',
+  5: 'Eliminar un alumno aleatoriamente de la clase.',
+  6: 'Mostrar por consola todos los datos de los alumnos que son chicas.',
+  7: 'Mostrar por consola el número de chicos y chicas que hay en la clase.',
+  8: 'Mostrar true o false por consola si todos los alumnos de la clase son chicas.',
+  9: 'Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.',
+  10: 'Añadir un alumno nuevo',
+  11: 'Mostrar por consola el nombre de la persona más joven de la clase.',
+  12: 'Mostrar por consola la edad media de todos los alumnos de la clase.',
+  13: 'Mostrar por consola la edad media de las chicas de la clase.',
+  14: 'Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10)',
+  15: 'Ordenar el array de alumnos alfabéticamente según su nombre.'
 }
 
 const actionRequirements = {
@@ -52,7 +41,7 @@ const actionRequirements = {
   '2': fullStudentsLength,
   '3': allStudentsName,
   '4': deleteLastStudent,
-  '5': deleteRandomStudent, 
+  '5': deleteRandomStudent,
   '6': eachGirlData,
   '7': totalPerGender,
   '8': allGirlsBool,
@@ -65,40 +54,50 @@ const actionRequirements = {
   '15': sortAlphabetically
 }
 
-// El usuario debe pulsar el número correspondiente a ese requisito para que se ejecute. 
+// aparecerá el listado completo de requisitos.
+function printRequirements() {
+  for (const key of Object.keys(Requisitos)) {
+    console.table(key + " : " + Requisitos[key])
+  }
+}
+
 // configuramos la utilidad de node para que los datos se pidan y se muestren por consola.
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  input: process.stdin,
+  output: process.stdout
+});
 
-  function selectAction() {
-    const promise = new Promise((resolve, reject) => {
-      // nos permite hacer una pregunta por consola al usuario. Ojo que es un proceso asíncrono.
-      rl.question('Introduce el número: ', (num) => {
-        rl.pause();
-        resolve(num)
-        // si el usuario mete un número, resolvemos la promesa con ese número.
-        // si el usuario mete una letra, debemos rechazar/rejectear la promesa.
-      })
+function selectAction() {
+  const promise = new Promise((resolve, reject) => {
+    rl.question('Introduce el número: ', (num) => {
+      rl.pause();
+      resolve(num)
     })
-    return promise
-  }
+  })
+  return promise
+}
 // consumidor
-async function fecthSelectedAction(){
-  try{
+async function fecthSelectedAction() {
+  try {
     let action = await selectAction()
     for (let key of Object.keys(actionRequirements)) {
-      if ((action) == key){
+      if ((action) == key) {
         actionRequirements[key]()
       }
-    }
-  }
-  // review this to add the finish process action.
-  catch(error){
+    } return action
+  } catch (error) {
     console.log(error)
   }
 }
 
-    
-fecthSelectedAction()
+
+printRequirements()
+let action = await fecthSelectedAction()
+while (action !== 0) {
+  if (action >= 1 && action <= 15) {
+    printRequirements()
+    action = await fecthSelectedAction()
+  } else {
+    break
+  }
+}
